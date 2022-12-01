44ad024694df9ccc871f1d045bf117c76ae72321
malinkacat <gumnickajaliza@gmail.com>
2022-12-01 12:51:04 +0200

    update

--- final/script.js
+++ final/script.js
@@ -1,11 +1,21 @@
 let button = document.getElementById("to-top-button");
 window.onscroll = function() {scrollFunction()};
+let navbar_bg = document.getElementById("nav-background");
+let navbar = document.getElementById("nav-container");
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     button.style.display = "block";
+    navbar.style.height = "60px";
+    navbar_bg.style.height = "60px";
+    navbar.style.transition = "0.5s ease-out";
+    navbar_bg.style.transition = "0.5s ease-out";
   } else {
     button.style.display = "none";
+    navbar.style.height = "80px";
+    navbar_bg.style.height = "80px";
+    navbar.style.transition = "0.5s ease-out";
+    navbar_bg.style.transition = "0.5s ease-out";
   }
 }
 
